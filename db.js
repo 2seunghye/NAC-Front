8f12508b9bd7e5d5db0d28e965b6918884889a90
2seunghye <tmdgp970120@naver.com>
2020-07-10 23:53:05 +0900

    Add Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:4342,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 970120,
+            name: "seunghye",
+            email: "tmdgp97120@las.com"
+        }
+    },
+    {
+        id:4380,
+        title: 'Video nice',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 970120,
+            name: "seunghye",
+            email: "tmdgp97120@las.com"
+        }
+    },
+    {
+        id:9322,
+        title: 'Video perfect',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 970120,
+            name: "seunghye",
+            email: "tmdgp97120@las.com"
+        }
+    },
+    {
+        id:4545,
+        title: 'Video good',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 970120,
+            name: "seunghye",
+            email: "tmdgp97120@las.com"
+        }
+    }
+]
