d30a6b365c00b9966f0e07896ba67b6872579c76
2seunghye <tmdgp970120@naver.com>
2020-07-28 01:03:12 +0900

    github login 1

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import mongoose from "mongoose";
 import session from "express-session";
+import MongoStore from "connect-mongo"
 import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -15,6 +17,8 @@ import routes from "./routes";
 import "./passport";
 
 const app = express();
+
+const CokiesStore = MongoStore(session)
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -27,9 +31,10 @@ app.use(morgan("dev"));
 app.use(session({
     secret: process.env.COOKIE_SECRET,
     resave: true,
-    saveUninitialized: false
-
-}));
+    saveUninitialized: false,
+    store: new CokiesStore({mongooseConnection: mongoose.connection})
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,19 +1,29 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
 import { home, search } from "../controller/videoController";
-import { postJoin, getJoin, logout, postLogin, getLogin } from "../controller/userController";
+import { postJoin, getJoin, logout, postLogin, getLogin, githubLogin, postGithubLogIn } from "../controller/userController";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin, postLogin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.gitHub, githubLogin);
+
+globalRouter.get(
+    routes.gitHubCallback, 
+    passport.authenticate("github", { failureRedirect: "/login"}),
+    postGithubLogIn
+);
 
 export default globalRouter;
